7a30bf7a6c87319335f3935d5a7715f0992bfc06
root <root@iPad-Doyle>
2024-03-08 14:19:02 +1030

    Hotfix 4

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,9 +1,8 @@
 #include <cmath>
-
 int binary_to_int(int binary_digits[], int number_of_digits){
     int num = 0;
-    for(int i = number_of_digits-1; i>=0; i--){
-        num += pow(binary_digits[i]*2, i);
+    for(int i = 0; i<number_of_digits; i++){
+        num += pow(binary_digits[number_of_digits-i-1]*2, i);
     }
     return num;
 }
