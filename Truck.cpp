5402e71f5eef3ca29700b7467d9dcbeef161fe96
root <root@iPad-Doyle>
2024-04-05 18:36:25 +1030

    kys

--- Truck.cpp
+++
@@ -1,3 +0,0 @@
-#include "Truck.h"
-
-Truck::Truck(int b_code, std::string b_name) : code(b_code), name(b_name){}
--- warrior.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "warrior.h"
-
-using namespace std;
-
-void Warrior::swingWeapon(Player* opponent){
-    cout << name << " swings their " << weapon << " at " << opponent->getName() << "!\n";
-    attack(opponent, damage);
-}
