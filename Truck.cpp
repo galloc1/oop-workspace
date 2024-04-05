232fea3191b677d6537cb48517d7a167b3e7e5e3
root <root@iPad-Doyle>
2024-04-05 13:41:26 +1030

    ugh

---
+++ Truck.cpp
@@ -0,0 +1,3 @@
+#include "Truck.h"
+
+Truck::Truck(int b_code, std::string b_name) : code(b_code), name(b_name){}
--- Truck.h
+++ Truck.h
@@ -9,7 +9,7 @@ class Truck{
     public:
         Truck() : code(0), name("") {}
 
-        Truck(int b_code, std::string b_name) : code(b_code), name(b_name) {}
+        Truck(int b_code, std::string b_name);
 
         std::string get_brand_name() {return name;}
         int get_brand_code() {return code;}
