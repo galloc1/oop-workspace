2565f6355f66efe5d8f368cfe46b375452e64a41
root <root@iPad-Doyle>
2024-04-24 21:21:01 +0930

    a

--- House.cpp
+++ House.cpp
@@ -1,8 +1,9 @@
 #include "House.h"
+#include <iostream>
 
 House::House() : appliances(new Appliance*[0]), numAppliances(0), currentSize(0){}
 House::House(int numAppliances) : numAppliances(numAppliances), appliances(new Appliance*[numAppliances]), currentSize(0){}
 
 bool House::addAppliance(Appliance* appliance){if(currentSize<numAppliances){appliances[currentSize]=appliance;currentSize++;return true;}else{return false;}}
 
-double House::getTotalPowerConsumption(){double total = 0;for(int i=0;i<currentSize-1;i++){total+=appliances[i]->getPowerConsumption();}return total;}
+double House::getTotalPowerConsumption(){double total = 0;for(int i=0;i<currentSize-1;i++){total+=appliances[i]->getPowerConsumption();}std::cout << currentSize; return total;}
