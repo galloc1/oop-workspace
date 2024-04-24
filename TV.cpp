f6b6aaf59ee6cac1c320d18a7d07de63e23436fb
root <root@iPad-Doyle>
2024-04-24 21:12:46 +0930

    I understand git now kinda

--- TV.cpp
+++ TV.cpp
@@ -1,9 +1,10 @@
 #include "TV.h"
+#include <iostream>
 
 TV::TV() : Appliance(), screenSize(0){}
-TV::TV(int powerRating, double screenSize) : Appliance(powerRating), screenSize(screenSize){}
+TV::TV(int powerRating, double screenSize) : Appliance(powerRating), screenSize(screenSize){std::cout<<"Power rating: "<<powerRating << std::endl << "Screen size: "<<screenSize<<std::endl;}
 
-double TV::getPowerConsumption(){double powerConsumption = powerRating*(screenSize/10.0); return powerConsumption;}
+double TV::getPowerConsumption(){double powerConsumption = powerRating*(screenSize/10.0); std::cout << "Power Consumption: "<<powerConsumption<<std::endl; return powerConsumption;}
 
 double TV::getScreenSize(){return screenSize;}
 
