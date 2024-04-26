4808777545377438161ba9afb1c4872f86c66e8d
root <root@iPad-Doyle>
2024-04-26 13:04:34 +0930

    Removed accidental files.

--- Fridge.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fridge.h"
-
-Fridge::Fridge() : Appliance(), volume(0){}
-Fridge::Fridge(int powerRating, double volume) : Appliance(powerRating), volume(volume){}
-
-void Fridge::setVolume(double volume){this->volume = volume;}
-double Fridge::getVolume(){return volume;}
-
-double Fridge::getPowerConsumption(){double powerConsumption = powerRating*24.0*(volume/100.0); return powerConsumption;}
--- House.cpp
+++
@@ -1,8 +0,0 @@
-#include "House.h"
-
-House::House() : appliances(new Appliance*[0]), numAppliances(0), currentSize(0){}
-House::House(int numAppliances) : numAppliances(numAppliances), appliances(new Appliance*[numAppliances]), currentSize(0){}
-
-bool House::addAppliance(Appliance* appliance){if(currentSize<numAppliances){appliances[currentSize]=appliance;currentSize++;return true;}else{return false;}}
-
-double House::getTotalPowerConsumption(){double total = 0;for(int i=0;i<currentSize;i++){total+=appliances[i]->getPowerConsumption();} return total;}
